c3c13ef2199f31ca038af4159e5b83b0a4d0b1ae
Cihangir SAVAS <cihangir@koding.com>
2013-05-19 04:05:00 -0700

    [neo4j] created neo4j main package

---
+++ neo4j.go
@@ -0,0 +1,35 @@
+package neo4j
+
+import (
+	"io/ioutil"
+	"net/http"
+	"strconv"
+)
+
+type Noe4j struct {
+	Client          *http.Client
+	BaseUrl         string
+	NodeUrl         string
+	RelationshipUrl string
+	IndexNodeUrl    string
+}
+
+func Connect(host string, port int) *Noe4j {
+	if host == "" {
+		host = "http://127.0.0.1"
+	}
+
+	if port == 0 {
+		port = 7474
+	}
+
+	url = host + ":" + strconv.Itoa(port)
+
+	return &Noe4j{
+		Client:          http.DefaultClient,
+		BaseUrl:         url + "/db/data",
+		NodeUrl:         BaseUrl + "/node",
+		IndexNodeUrl:    BaseUrl + "/index/node",
+		RelationshipUrl: BaseUrl + "/relationship",
+	}
+}
