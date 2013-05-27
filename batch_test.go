f6f301861b1aa33d89ad7c544c83c84645dedebf
Cihangir SAVAS <cihangir@koding.com>
2013-05-27 00:51:49 -0700

    [batch][test] added test file

--- batch_test.go
+++ batch_test.go
@@ -1,17 +1,65 @@
 package neo4j
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestBatch(t *testing.T) {
-	neo4jConnection := Connect("", 0)
-	batch := neo4jConnection.NewBatch()
+	// neo4jConnection := Connect("", 0)
+	// batch := neo4jConnection.NewBatch()
 
-	node := &Node{}
-	relationship := &Relationship{}
+	// node := &Node{}
+	// data := make(map[string]interface{})
+	// data["hede"] = "debe"
+	// node.Data = data
 
-	batch.Get(node).Create(node).Delete(node).Update(node).Get(relationship).Create(relationship).Delete(relationship).Update(relationship).Execute()
-	fmt.Println("...")
+	// node2 := &Node{}
+	// data2 := make(map[string]interface{})
+	// data2["dede"] = "baba"
+	// node2.Data = data2
+
+	// node3 := &Node{}
+	// data3 := make(map[string]interface{})
+	// data3["fasdfasfd"] = "90q4jfpoqiwe"
+	// node3.Data = data3
+	// node3.Id = "691"
+
+	// relationship := &Relationship{}
+	// data4 := make(map[string]interface{})
+	// data4["hede"] = "naber"
+	// relationship.Data = data4
+	// relationship.Type = "denemerelationshipType"
+	// relationship.StartNodeId = "649"
+	// relationship.EndNodeId = "677"
+
+	// relationship2 := &Relationship{}
+	// data4["hedefsdfa"] = "adfasdfnaber"
+	// relationship2.Data = data4
+	// relationship2.StartNodeId = "463"
+	// relationship2.EndNodeId = "503"
+	// relationship2.Type = "denemerelationshipType"
+
+	// relationship3 := &Relationship{}
+	// data4["hedefsdfa"] = "naberrrrrrrrrrrrr"
+	// relationship3.Id = "1037"
+	// relationship3.Data = data4
+
+	// manuelBatchRequest := &ManuelBatchRequest{}
+	// manuelBatchRequest.Body = data
+	// // batch.Get(node).Create(node).Delete(node)
+	// batch.Create(node)
+	// batch.Create(relationship)
+	// batch.Get(relationship3)
+	// batch.Update(relationship3)
+	// batch.Delete(relationship3)
+	// // batch.Create(relationship2)
+	// batch.Create(node2)
+	// batch.Get(node3)
+	// // manuelBatchRequest.To = "{" + batch.GetLastIndex() + "}/relationship"
+	// res, err := batch.Execute()
+	// fmt.Println(res, err)
+
+	// fmt.Println("naber")
+	// fmt.Println(node.Id)
+	// fmt.Println(relationship.Id)
 }
